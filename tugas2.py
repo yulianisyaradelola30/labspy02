e095e37854cfacc6496825ea44a4a7c193c746b3
yulianisyaradelola30 <yulianisyaradelola30>
2019-11-03 12:09:42 +0700

    first python

---
+++ tugas2.py
@@ -0,0 +1,10 @@
+a=int(input("bilangan A = "))
+b=int(input("bilangan B = "))
+c=int(input("bilangan C = "))
+
+if a>b and a>c:
+    print (a ,'lebih besar dari pada B dan C')
+elif b>a and b>c:
+    print (b ,'lebih besar dari pada A dan C')
+else :
+     print (c ,'lebih besar dari A dan B')
